1aaa4596eaa5cb312985547a8bb40fd0cea9274e
Juan Manuel Zimmermann <jzimmermann@stenfar.com.ar>
2024-06-29 00:03:17 -0300

    t9

--- t9/t9.py
+++ t9/t9.py
@@ -1,6 +1,4 @@
-def lockMoves(n1: str, n2: str, unlocked: list) -> int:
-    if n2 in unlocked:
-        return 0
+def lockMoves(n1: str, n2: str) -> int:
     # Convert numbers to strings for easy digit access
     str_n1, str_n2 = n1.zfill(4), n2.zfill(4)
     total_moves = 0
@@ -13,6 +11,38 @@ def lockMoves(n1: str, n2: str, unlocked: list) -> int:
         moves = min(diff, 10 - diff)
         total_moves += moves
     return total_moves
+
+
+def costs(n: int, edges: list, nodes: list):
+    for v in range(n):
+        for w in range(v, n):
+            if v != w:
+                cost = lockMoves(nodes[v], nodes[w])
+                print(f"Cost from {nodes[v]} to {nodes[w]}: {cost}")
+                edges[v][w] = cost
+                edges[w][v] = cost
+
+
+def min_edge(nodes: list, edges: list, visited: list):
+    min = (0, 0, 1000)
+    for v in visited:
+        for w in range(len(nodes)):
+            if w not in visited:
+                if edges[nodes.index(v)][w] < min[2]:
+                    min = (nodes.index(v), w, edges[nodes.index(v)][w])
+    return min
+
+
+def prim(n: int, nodes: list, edges: list):
+    visited = [nodes[0]]
+    edges_visited = []
+    tree = []
+    while len(visited) < n:
+        e = min_edge(visited, edges, visited)
+        visited.append(nodes[e[1]])
+        edges_visited.append(e)
+        tree.append((e[0], e[1]))
+    return tree
 
 
 def minMoves(n: int, codes: list) -> int:
@@ -37,11 +67,19 @@ def minMoves(n: int, codes: list) -> int:
 cases: int = int(input())
 answers: list = []
 
-for i in range(cases):
-    line: str = input().split(' ')
+for _ in range(cases):
+    line: str = input().split(" ")
     n: int = int(line[0])
-    codes: list = line[1:]
-    answers.append(minMoves(n, codes))
+    nodes: list = line[1:]
+    edges: list = [[0] * n for _ in range(n)]  # Matrix
+
+    costs(n, edges, nodes)
+    for i in range(n):
+        print(edges[i])
+
+    tree: list = prim(n, nodes, edges)
+    print(tree)
+    nodes.append("0000")
 
 for i in range(len(answers)):
     print(answers[i])
